55402333d33b3256318f03de4f0f1760d25a1616
eas27 <80135807+eas27@users.noreply.github.com>
2023-11-30 10:46:31 -0500

    Adding Utility Files

---
+++ ChemUtilities.py
@@ -0,0 +1,115 @@
+import pandas as pd
+import tqdm
+from rdkit import Chem
+from rdkit.Chem import SaltRemover
+import Enumerate
+
+def SaltStripMolecules (molecules: pd.DataFrame, smilescol='SMILES', neutralize = True):
+    tqdm.pandas ()
+    print ('salt stripping')
+    molecules[smilescol] = molecules[smilescol].progress_apply(lambda smi:SaltStrip(smi, neutralize))
+    print ('completed salt stripping')
+    return molecules
+
+def SaltStrip (molec, neutralize = True):
+    try:
+        saltstrip = SaltRemover.SaltRemover()
+        if type(molec) != Chem.Mol:
+            m = Chem.MolFromSmiles(molec)
+        else:
+            m = molec
+        m=  saltstrip.StripMol(m)
+        smi = Chem.MolToSmiles(m, kekuleSmiles = False)
+        smi = Neutralize(smi)
+    except:
+        return molec
+    return smi
+
+def Neutralize (smi):
+    try:
+        m = Chem.MolFromSmiles(smi)
+    except:
+        return smi
+    pattern = Chem.MolFromSmarts("[+1!h0!$([*]~[-1,-2,-3,-4]),-1!$([*]~[+1,+2,+3,+4])]")
+    at_matches = m.GetSubstructMatches(pattern)
+    at_matches_list = [y[0] for y in at_matches]
+    try:
+        if len(at_matches_list) > 0:
+            for at_idx in at_matches_list:
+                atom = m.GetAtomWithIdx(at_idx)
+                chg = atom.GetFormalCharge()
+                hcount = atom.GetTotalNumHs()
+                atom.SetFormalCharge(0)
+                atom.SetNumExplicitHs(hcount - chg)
+                atom.UpdatePropertyCache()
+
+        smi = Chem.MolToSmiles(m, kekuleSmiles=False)
+        return smi
+    except:
+        print (smi)
+        return smi
+
+def Deprotect (compound, rxnscheme, deprotect_name=None, iterate = False, retsmiles = False):
+    deprotected = False
+    Enumerator = Enumerate.Enumerate ()
+    first_round = True
+
+    has_scheme = Enumerator.ReadRxnScheme(rxnscheme, 'Deprotection',  verbose=True)
+    schemeinfo = [[{"Reactants":["r0","None"],"Rxns":{"default":[]}}], ["r0","None"] ]
+    if deprotect_name is None:
+        for k,v in has_scheme[0][0]['Rxns'].items ():
+            if type(v) is list:
+                for l in v:
+                    schemeinfo[0][0]['Rxns']["default"].append(l)
+            else:
+                schemeinfo[0][0]['Rxns']["default"].append(v)
+    else:
+        if type (deprotect_name) is str:
+            deprotect_name = [deprotect_name]
+        for d in deprotect_name:
+            v = has_scheme[0][0]['Rxns'] [d]
+            if type(v) is list:
+                for l in v:
+                    schemeinfo[0][0]['Rxns']["default"].append(l)
+
+    if not has_scheme[0] is  None:
+        last_dp = False
+        while first_round or  (iterate and last_dp == True):
+            res, prod_ct, resinfo = Enumerator.RunRxnScheme([compound, 'None'], None,None, False, schemeinfo )
+            if (res != 'FAIL' and res != 'None'):
+                if type(res) == str:
+                    compound = Chem.MolFromSmiles(res)
+                deprotected = True
+                last_dp = True
+            else:
+                last_dp = False
+
+            first_round = False
+
+        first_round = True
+        last_dp = False
+        res_list = [compound]
+
+    has_scheme = Enumerator.ReadRxnScheme(rxnscheme, 'Deprotect_KeepBoth', False)
+    if has_scheme[0] is not None:
+        while first_round or (iterate and last_dp == True):
+            res, prod_ct = Enumerator.RunRxnScheme([compound, 'None'], rxnscheme, 'Deprotect_KeepBoth', False)
+            if res == 'NOSCHEMEFAIL':
+                break
+            if (res != 'NOSCHEMEFAIL' and res != 'FAIL' and res != 'None'):
+                res_list.append (Chem.MolFromSmiles(res))
+                compound = Chem.MolFromSmiles(res)
+                deprotected = True
+                last_dp = True
+            else:
+                last_dp = False
+            first_round = False
+
+
+    res = res_list
+    if retsmiles:
+        if deprotected:
+            return Chem.MolToSmiles(res[0])
+        else:
+            return compound
+    return res, deprotected
---
+++ MolDisplay.py
@@ -0,0 +1,123 @@
+from rdkit import Chem
+from rdkit.Chem import Draw
+import matplotlib.pyplot as plt
+import io
+import numpy as np
+from PIL import Image
+import base64
+import streamlit as st
+from st_aggrid import AgGrid, JsCode
+from st_aggrid.grid_options_builder import  GridOptionsBuilder
+from streamlit import runtime
+import pandas as pd
+
+
+def ShowMol( smiles, outtype=None):
+    if smiles is not None:
+        try:
+            m = Chem.MolFromSmiles(smiles)
+            img = Draw.MolToImage(m, size=(400, 400))
+        except Exception as eq:
+            img = np.zeros([100, 100, 3], dtype=np.uint8)
+            img.fill(255)  # or img[:] = 255
+            img = Image.fromarray(img)
+    else:
+        img = np.zeros([100, 100, 3], dtype=np.uint8)
+        img.fill(255)  # or img[:] = 255
+    img.save('test.png')
+    if outtype == 'b64_datauri':
+        buf = io.BytesIO()
+        img.save(buf, format='png')
+        buf.seek(0)
+        data = buf.read()
+        sb = ''
+        sb += "data:image/png;base64,"
+        sb += base64.b64encode(data).decode('ascii')
+        return sb
+    return plt
+
+def ShowMols( smiles_list, cols = 4, subImgSize = (200,200), outtype = 'plt', fname='test_mols.png'):
+    list = []
+    if smiles_list is not None:
+        for smi in smiles_list:
+            if smi is not None and smi != 'None' and smi != 'NOSMILES':
+                if not (isinstance(smi, Chem.rdchem.Mol)):
+                    try:
+                        m = Chem.MolFromSmiles(smi)
+                    except:
+                        m = Chem.MolFromSmiles ('')
+                    list.append(m)
+                else:
+                    list.append (smi)
+
+        img = Draw.MolsToGridImage(list, molsPerRow=cols, subImgSize=subImgSize )
+
+        if outtype == 'bytesio':
+            buf = io.BytesIO()
+            img.save(buf, format='png')
+            buf.seek(0)
+            return buf.read()
+        elif outtype == 'b64_datauri':
+            buf = io.BytesIO()
+            img.save(buf, format='png')
+            buf.seek(0)
+            data = buf.read()
+            sb = ''
+            sb += "data:image/png;base64,"
+            sb += base64.b64encode(data).decode('ascii')
+            return sb
+        else:
+            img.save(fname)
+            plt.imshow(img)
+            return plt
+    else:
+        return None
+    
+def ShowMols_StreamLit_Grid (df, smilescols = ['SMILES'], rowheight = 100):
+    dispdf = df
+    for s in smilescols:
+        dispdf [s + '_Image'] = None
+    image_render = JsCode("""function (params) {
+                         var imageElement = document.createElement("img");
+                         return imageElement;
+                         }""")
+
+    gb = GridOptionsBuilder()
+    for c in df.columns:
+        if c not in smilescols:
+            gb.configure_column(c, headerName=c, width=50)
+        else:
+            gb.configure_column(c + "_Image", headerName=s, cellRenderer=image_render)
+    for ix, row in dispdf.iterrows():
+        for s in smilescols:
+            print (row[s])
+            if row[s] is not None:
+                 dispdf.at [ix,s + '_Image'] = ShowMol(row[s],outtype='b64_datauri')
+            else:
+                 dispdf.at[ix, s + '_Image'] = ""
+
+    gb.configure_default_column(groupable=True, value=True, enableRowGroup=True, editable=True,
+                                enableRangeSelection=True, )
+
+    gridOptions = gb.build()
+    gridOptions['rowHeight'] = rowheight
+
+    AgGrid(dispdf, allow_unsafe_jscode=True, enable_enterprise_modules=True, gridOptions=gridOptions, height=500)
+    return
+
+class Chem_ShowMols_UI:
+    def body(self):
+        df = pd.DataFrame ([['CCCC', 'c1ccccc1', 5], ['CCNC1CCCC1', 'C1=NCC=N1', 10]], columns=['SMILES','SMILES2',  'Test'])
+        st.markdown("""<h1 style='text-align: center; margin-bottom: -35px;'>
+               MolDisplay</h1>""", unsafe_allow_html=True)
+
+        ShowMols_StreamLit_Grid (df, ['SMILES','SMILES2'])
+
+
+    def RunUI(self):
+        self.body()
+
+if __name__ == "__main__":
+    if runtime.exists():
+        csm = Chem_ShowMols_UI()
+        csm.RunUI()
